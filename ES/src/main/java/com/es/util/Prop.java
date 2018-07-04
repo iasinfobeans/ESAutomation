fe8264df8e1e9e1b06470a3d08474118856aa83f
Pankaj Shukla <pankajs@surveymonkey.com>
2018-07-04 22:19:05 +0530

    Added platform independent paths

--- ES/src/main/java/com/es/util/Prop.java
+++ ES/src/main/java/com/es/util/Prop.java
@@ -3,6 +3,7 @@ package com.es.util;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Properties;
 
 import org.apache.log4j.Logger;
@@ -10,16 +11,17 @@ import org.apache.log4j.Logger;
 public class Prop {
 
 	private static Logger log = Logger.getLogger(Prop.class.getName());
+	private static String configPropertiesFilePath = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "properties", "config.properties").toString();
+	private static String testDataPropertiesFilePath = Paths.get(System.getProperty("user.dir"), "src", "main", "resources", "properties", "testData.properties").toString();
 
 	public static String getConfigValue(String key) throws IOException {
 		FileInputStream fis;
 		Properties property = null;
 		try {
-			fis = new FileInputStream(
-					System.getProperty("user.dir") + "\\src\\main\\resources\\properties\\config.properties");
+			fis = new FileInputStream(configPropertiesFilePath);
 			property = new Properties();
 			property.load(fis);
-			log.info("config file loaded");
+			log.info("Loaded config file");
 		} catch (FileNotFoundException e) {
 			log.info("Failed to load testdata file");
 		} catch (IOException e) {
@@ -32,12 +34,10 @@ public class Prop {
 		FileInputStream fis;
 		Properties property = null;
 		try {
-			fis = new FileInputStream(
-					System.getProperty("user.dir") + "\\src\\main\\resources\\properties\\testData.properties");
+			fis = new FileInputStream(testDataPropertiesFilePath);
 			property = new Properties();
-			
 			property.load(fis);
-			log.info("test data file loaded");
+			log.info("Loaded test data file");
 		} catch (FileNotFoundException e) {
 			log.info("Failed to load testdata file");
 		} catch (IOException e) {
