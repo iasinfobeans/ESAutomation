d36e836ca78b4756f97f9b2c40a4967c662cf8a5
Pankaj Shukla <pankajs@surveymonkey.com>
2018-07-10 00:30:24 +0530

    Removed webdriver variable

--- ES/src/main/java/com/es/pom/MyProfilePage.java
+++ ES/src/main/java/com/es/pom/MyProfilePage.java
@@ -5,42 +5,35 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
-public class MyprofilePage {
+public class MyProfilePage {
 
-	private static Logger log = Logger.getLogger(MyprofilePage.class.getName());
-	
-	 private WebDriver driver;
-	/* public RegisterPage(WebDriver driver){
-	   this.driver=driver;
-	   PageFactory.initElements(driver,this);
+	private static Logger log = Logger.getLogger(MyProfilePage.class.getName());
+
+	@FindBy(xpath="//*[@title=\"Quotations\"]")
+	static WebElement quotationOption;
+
+	@FindBy(xpath="//*[@title=\"Reports\"]")
+	static WebElement reportOption;
+
+
+	@FindBy(xpath="//*[@title=\"Invoices\"]")
+	static WebElement invoicesOption;
+
+	public static void portalOptionForQualifiedUser() {
+
+		Assert.assertTrue(quotationOption.isDisplayed());
+		log.info("Verify Quotation Option displayed");
+
+		Assert.assertTrue(reportOption.isDisplayed());
+		log.info("Verify Report Option displayed");
+
+		Assert.assertTrue(invoicesOption.isDisplayed());
+		log.info("Verify Invoices Option displayed");
 	}
-	*/
-	 
-	 @FindBy(xpath="//*[@title=\"Quotations\"]")
-		static WebElement quotationOption;
-	 
-	 @FindBy(xpath="//*[@title=\"Reports\"]")
-	  static WebElement reportOption;
-	 
-	
-	 @FindBy(xpath="//*[@title=\"Invoices\"]")
-	  static WebElement invoicesOption;
-  
-	 public static void portalOptionForQualifiedUser() {
-		 
-		 Assert.assertTrue(quotationOption.isDisplayed());
-		 log.info("Verify Quotation Option displayed");
-		 
-		 Assert.assertTrue(reportOption.isDisplayed());
-		 log.info("Verify Report Option displayed");
-		 
-		 Assert.assertTrue(invoicesOption.isDisplayed());
-		 log.info("Verify Invoices Option displayed");
-}
-	 
-  }	 
-	 
-	 
-	 
-	 
+
+}	 
+
+
+
+
 
