a174d05541761ea02d4e3c7c484b27a14bbaaeed
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-07-17 12:42:06 +0530

    Modified registration code

--- ES/src/main/java/com/es/pom/RegisterPage.java
+++ ES/src/main/java/com/es/pom/RegisterPage.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import io.qameta.allure.Step;
 import com.es.util.CommonUtils;
-import com.es.util.Yopmail;
 
 public class RegisterPage {
 
