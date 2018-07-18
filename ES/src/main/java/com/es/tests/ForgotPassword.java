ed774209c87022b0d795acbe42609fe75ab645ef
iasinfobeans <shefali.garg@infobeans.com>
2018-07-18 13:15:31 +0530

    Added test case for verify reset email body and Reset password using reset email.

--- ES/src/main/java/com/es/tests/ForgotPassword.java
+++ ES/src/main/java/com/es/tests/ForgotPassword.java
@@ -3,10 +3,13 @@ package com.es.tests;
 import java.io.IOException;
 import org.testng.annotations.Test;
 import com.es.pom.ForgotPasswordPage;
+import com.es.pom.ResetPasswordPage;
 import com.es.pom.SignInPage;
 import com.es.setup.Setup;
 import com.es.util.Prop;
 import com.es.util.SeleniumUtils;
+import com.es.util.Yopmail;
+
 import io.qameta.allure.Description;
 
 public class ForgotPassword extends Setup {
@@ -23,6 +26,34 @@ public class ForgotPassword extends Setup {
 			throw e;
 		}
 	}
+	
+	@Test(groups = {"smoke"})
+	@Description("Verify that an email with password reset link is sent to the customer")
+	public static void verifyPasswordResetMailisSent() throws IOException, InterruptedException {
+		try{
+			SignInPage.navigateToForgotPassword();
+			ForgotPasswordPage.resetPassword(Prop.getTestData("username"));
+			Yopmail.verifyPassResetEmailBody(Prop.getTestData("username"));
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyForgotPasswordFlow");
+			e.getStackTrace();
+			throw e;
+		}
+	}
 
+	@Test(groups = {"smoke"})
+	@Description("Verify that customer is able to reset the password after clicking the link in the email")
+	public static void verifydResetPasswordFromMail() throws IOException, InterruptedException {
+		try{
+			SignInPage.navigateToForgotPassword();
+			ForgotPasswordPage.resetPassword(Prop.getTestData("username"));
+			String resetPasswordLink = Yopmail.getPasswordResetLink(Prop.getTestData("username"));
+			ResetPasswordPage.changePassword(resetPasswordLink);
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyForgotPasswordFlow");
+			e.getStackTrace();
+			throw e;
+		}
+	}
 
 }
