f2d5414db15a6247a2c451585fe2efff8d37c921
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-07-19 17:20:44 +0530

    Removed unused import

--- ES/src/main/java/com/es/pom/ApplicationPage.java
+++ ES/src/main/java/com/es/pom/ApplicationPage.java
@@ -1,10 +1,8 @@
 package com.es.pom;
 
 import org.apache.log4j.Logger;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
 
 import io.qameta.allure.Step;
