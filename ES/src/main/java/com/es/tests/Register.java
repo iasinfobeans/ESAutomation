8e14d0e513a1742ad553364a74eacb63fb9b0757
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-07-23 12:11:09 +0530

    Added try catch

--- ES/src/main/java/com/es/tests/Register.java
+++ ES/src/main/java/com/es/tests/Register.java
@@ -5,6 +5,7 @@ import com.es.pom.RegisterPage;
 import com.es.pom.SignInPage;
 import com.es.setup.Setup;
 import com.es.util.Prop;
+import com.es.util.SeleniumUtils;
 import com.es.util.Yopmail;
 import io.qameta.allure.Description;
 
@@ -52,42 +53,72 @@ public class Register extends Setup{
 	@Test(groups = { "smoke" })
 	@Description("Verify that the user is able to initiate the registration process by clicking on the register link present on the home page.")
 	public void verifyRegistrationFromPortal() throws InterruptedException {
-		SignInPage.navigateToNormalRegistration() ;
-		RegisterPage.enterEmailInRegistration();
+		try{
+			SignInPage.navigateToNormalRegistration() ;
+			RegisterPage.enterEmailInRegistration();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyRegistrationFromPortal");
+			e.getStackTrace();
+			throw e;
+		}
 	}
 
 	@Test(groups = { "smoke"})
 	@Description("Verify the email for OTP.")
 	public void verifyEmailForOTP() throws InterruptedException {
-		SignInPage.navigateToNormalRegistration();
-		String email = RegisterPage.enterEmailInRegistration();
-		Yopmail.verifyOTPEmailBody(email);
+		try{
+			SignInPage.navigateToNormalRegistration();
+			String email = RegisterPage.enterEmailInRegistration();
+			Yopmail.verifyOTPEmailBody(email);
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyEmailForOTP");
+			e.getStackTrace();
+			throw e;
+		}
 	}
 
 	@Test(groups = { "smoke" })
 	@Description("Verify the registration process once correct OTP and other fields are entered.")
 	public void verifyRegistrationWithCorrectOTP() throws InterruptedException {
-		SignInPage.navigateToNormalRegistration();
-		RegisterPage.enterEmailInRegistration();
-		RegisterPage.enterPersonalInfoInRegistration(Prop.getTestData("firstName"), Prop.getTestData("lastName"),Prop.getTestData("companyName"),Prop.getTestData("phone"),Prop.getTestData("newPassword"),Prop.getTestData("confirmPassword"));
-		DashboardPage.verifyRegistrationProcessAfterEnteringCorrectOTP();
+		try{
+			SignInPage.navigateToNormalRegistration();
+			RegisterPage.enterEmailInRegistration();
+			RegisterPage.enterPersonalInfoInRegistration(Prop.getTestData("firstName"), Prop.getTestData("lastName"),Prop.getTestData("companyName"),Prop.getTestData("phone"),Prop.getTestData("newPassword"),Prop.getTestData("confirmPassword"));
+			DashboardPage.verifyRegistrationProcessAfterEnteringCorrectOTP();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyRegistrationWithCorrectOTP");
+			e.getStackTrace();
+			throw e;
+		}
 	}
 
 	@Test(groups = {"smoke"})
 	@Description("Verify the email notifications sent to the Customer upon successful registration.")
 	public void verifySuccessfulRegistrationEmailNotificationsToCustomer() throws InterruptedException {
-		SignInPage.navigateToNormalRegistration();
-		String email = RegisterPage.enterEmailInRegistration();
-		RegisterPage.enterPersonalInfoInRegistration(Prop.getTestData("firstName"), Prop.getTestData("lastName"),Prop.getTestData("companyName"),Prop.getTestData("phone"),Prop.getTestData("newPassword"),Prop.getTestData("confirmPassword"));
-		Yopmail.verifyRegistrationEmailBody(email);
+		try{
+			SignInPage.navigateToNormalRegistration();
+			String email = RegisterPage.enterEmailInRegistration();
+			RegisterPage.enterPersonalInfoInRegistration(Prop.getTestData("firstName"), Prop.getTestData("lastName"),Prop.getTestData("companyName"),Prop.getTestData("phone"),Prop.getTestData("newPassword"),Prop.getTestData("confirmPassword"));
+			Yopmail.verifyRegistrationEmailBody(email);
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifySuccessfulRegistrationEmailNotificationsToCustomer");
+			e.getStackTrace();
+			throw e;
+		}
 	}
- 
-	 // 5,6 and 7 are CRM Based 
-	
+
+	// 5,6 and 7 are CRM Based 
+
 	@Test(groups = { "smoke" })
 	@Description("Verify that the user is qualified and all the portal options get available to user.")
 	public void verifyUserIsQualified() throws InterruptedException {
-		SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
-		DashboardPage.verifyPortalOptionForQualifiedUser();
+		try{	
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			DashboardPage.verifyPortalOptionForQualifiedUser();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyUserIsQualified");
+			e.getStackTrace();
+			throw e;
+		}
 	}
 }
