79f07cee58a5e02df629418b7a6ffc8d21b219be
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-07-26 15:36:21 +0530

    Merged

--- ES/src/main/java/com/es/pom/CRMPage.java
+++ ES/src/main/java/com/es/pom/CRMPage.java
@@ -54,12 +54,15 @@ public class CRMPage {
 	@FindBy(xpath = "//*[@title='Sort by Email']")
 	static WebElement clickForSort;
 	
-<<<<<<< HEAD
-=======
 	@FindBy(xpath = "//*[@id='grid_refresh']")
 	static WebElement refreshLeadList;
 	
->>>>>>> branch 'master' of https://github.com/iasinfobeans/ESAutomation.git
+	@FindBy(xpath = "//*[text()='ES Portal']")
+	static WebElement esPortalOption;
+	
+	@FindBy(xpath = "//*[@id='navTabButtonUserInfoSignOutId']")
+	static WebElement signOutOption;
+	
 	@Step("Signing in CRM..")
 	public static void login(String username, String password) {
 		try {
@@ -82,80 +85,25 @@ public class CRMPage {
 			String findLead = "//*[text()='" + email + "']";
 			Actions act = new Actions(Setup.driver);
 			act.moveToElement(moveToSales).perform();
-<<<<<<< HEAD
-			Thread.sleep(20000);
-=======
 			SeleniumUtils.waitForElementToBeVisible(clickOnSales);
->>>>>>> branch 'master' of https://github.com/iasinfobeans/ESAutomation.git
 			act.moveToElement(clickOnSales).click().build().perform();
 			SeleniumUtils.switchToIframeByIndex(0);
-<<<<<<< HEAD
-			Thread.sleep(10000);
-			//*[@id="grid_refresh"]
-			WebElement refresh = Setup.driver.findElement(By.xpath("//*[@id='grid_refresh']"));
-			JavascriptExecutor jsrefresh = (JavascriptExecutor) Setup.driver;
-			jsrefresh.executeScript("arguments[0].click();", refresh);
-			
-			Thread.sleep(10000);
-=======
 			SeleniumUtils.waitForElementToBeVisible(refreshLeadList);
 			JavascriptExecutor jsrefresh = (JavascriptExecutor) Setup.driver;
 			jsrefresh.executeScript("arguments[0].click();", refreshLeadList);
-			
+			log.info("Refreshed Lead List.");
 			SeleniumUtils.waitForElementToBeVisible(searchFilter);
->>>>>>> branch 'master' of https://github.com/iasinfobeans/ESAutomation.git
 			searchFilter.sendKeys(name);
 			searchFilter.sendKeys(Keys.RETURN);
-<<<<<<< HEAD
-			Thread.sleep(10000);
-			clickForSort.click();
-			clickForSort.click();
-			Thread.sleep(10000);
-			
-			
-			WebElement ele = Setup.driver.findElement(By.xpath(findLead));
-=======
 			SeleniumUtils.waitForElementToBeVisible(clickForSort);
 			clickForSort.click();
 			clickForSort.click();			
 			
 			WebElement ele = Setup.driver.findElement(By.xpath(findLead));
 			SeleniumUtils.waitForElementToBeVisible(ele);
->>>>>>> branch 'master' of https://github.com/iasinfobeans/ESAutomation.git
 			JavascriptExecutor js = (JavascriptExecutor) Setup.driver;
 			js.executeScript("arguments[0].click();", ele);
-			
-			
-			/*WebElement selectView = Setup.driver.findElement(By.xpath("//*[@id='crmGrid_SavedNewQuerySelector']/span/img"));
-			JavascriptExecutor jsselectView = (JavascriptExecutor) Setup.driver;
-			jsselectView.executeScript("arguments[0].click();", selectView);
-			
-			Setup.driver.findElement(By.xpath("//*[text()='Create Personal View']")).click();
-			
-			for(String winHandle : Setup.driver.getWindowHandles()){
-				Setup.driver.switchTo().window(winHandle);
-			}
-			
-			Thread.sleep(20000);
-			Setup.driver.switchTo().frame(0);
-			Setup.driver.findElement(By.xpath("//*[@class='ms-crm-SelectBox']")).click();
-			Thread.sleep(20000);
-			Setup.driver.findElement(By.xpath("//*[@id='fld']/option[56]")).click();
-			WebElement filterEle = Setup.driver.findElement(By.xpath("//*[@id='filterButtonImage']"));
-			JavascriptExecutor jsfilter = (JavascriptExecutor) Setup.driver;
-			jsfilter.executeScript("arguments[0].click();", filterEle);
-			Thread.sleep(10000);
-			clickOnDropDown.click();
-			Thread.sleep(10000);
-			selNewToOld.click();
-			
-			
-			
-			Thread.sleep(20000);
-			WebElement ele = Setup.driver.findElement(By.xpath(findLead));
-			JavascriptExecutor js = (JavascriptExecutor) Setup.driver;
-			js.executeScript("arguments[0].click();", ele);*/
-
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
@@ -163,6 +111,16 @@ public class CRMPage {
 		}
 		qualify.click();
 		log.info("Qualified lead in CRM for "+name + "and for email id :"+email);
+	}
+
+	@Step("Signing Out From CRM..")
+	public static void logout() {
+		SeleniumUtils.switchToDefaultIframe();
+		SeleniumUtils.waitForElementToBeVisible(esPortalOption);
+		esPortalOption.click();
+		SeleniumUtils.waitForElementToBeVisible(signOutOption);
+		signOutOption.click();
+		log.info("Logged out from CRM.");
 	}
 
 }
