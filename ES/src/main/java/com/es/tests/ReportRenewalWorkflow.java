8960c510e17112dd198114d2625d74fd4c95986a
Pankaj Shukla <pankajs@surveymonkey.com>
2018-07-27 18:06:09 +0530

    Added verifyactionsAvailableHoveringOverAnyReport, verifyActionOnPayButton, verifyActionOnViewInvoicessButton, verifyActionOnViewInvoicesButton

--- ES/src/main/java/com/es/tests/ReportRenewalWorkflow.java
+++ ES/src/main/java/com/es/tests/ReportRenewalWorkflow.java
@@ -1,6 +1,10 @@
 package com.es.tests;
 import org.testng.annotations.Test;
 import com.es.pom.DashboardPage;
+import com.es.pom.InvoiceListingPage;
+import com.es.pom.OverlayPage;
+import com.es.pom.PaymentPage;
+import com.es.pom.ProjectListingPage;
 import com.es.pom.ReportsPage;
 import com.es.pom.SignInPage;
 import com.es.setup.Setup;
@@ -24,6 +28,68 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-}
+	@Test(groups = { "smoke" }) 
+	@Description("Verify the actions available on hovering over any report.")
+	public void verifyactionsAvailableHoveringOverAnyReport() throws InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			OverlayPage.skipoverlayPage();
+			DashboardPage.hoveringOverAnyReport();
+			DashboardPage.actionsAvailableHoveringOverAnyReport();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyactionsAvailableHoveringOverAnyReport");
+			e.getStackTrace();
+			throw e;
+		}
+	}
 
+	@Test(groups = { "smoke" })  
+	@Description("Verify the action on clicking the 'Pay' button")
+	public void verifyActionOnPayButton() throws InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			OverlayPage.skipoverlayPage();
+			DashboardPage.hoveringOverAnyReport();
+			DashboardPage.clickActionOnPayButton();
+			PaymentPage.verifyPaymentPage();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyActionOnPayButton()");
+			e.getStackTrace();
+			throw e;
+		}
+	}
+	
+	@Test(groups = { "smoke" })  
+	@Description("Verify the action on clicking the 'View Projects' button")
+	public void verifyActionOnViewInvoicessButton() throws InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			OverlayPage.skipoverlayPage();
+			DashboardPage.hoveringOverAnyReport();
+			DashboardPage.clickActionOnViewProjectsButton();
+			ProjectListingPage.verifyViewProjectsPage();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyActionOnViewInvoicessButton()");
+			e.getStackTrace();
+			throw e;
+		}
+	}
+	
+	@Test(groups = { "smoke" })  
+	@Description("Verify the action on clicking the 'View Invoices' button")
+	public void verifyActionOnViewInvoicesButton() throws InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			OverlayPage.skipoverlayPage();
+			DashboardPage.hoveringOverAnyReport();
+			DashboardPage.clickActionOnViewInvoicesButton();
+			InvoiceListingPage.verifyViewInvoicesPage();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyActionOnViewInvoicesButton(");
+			e.getStackTrace();
+			throw e;
+		}
+	}
+	
+}
 
