72b0776df1130a4f816adda6799634838f64613f
iasinfobeans <shefali.garg@infobeans.com>
2018-08-02 17:30:51 +0530

    Replace

--- ES/src/main/java/com/es/pom/PaymentPage.java
+++ ES/src/main/java/com/es/pom/PaymentPage.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
 import com.es.util.Prop;
+import com.es.util.RobotUtils;
 import com.es.util.SeleniumUtils;
 
 import io.qameta.allure.Step;
@@ -16,6 +17,42 @@ public class PaymentPage {
 
 	@FindBy(xpath="//div[@class='ehading']")
 	static WebElement paymentElement;
+
+	@FindBy(id="other_amount")
+	static WebElement payAmountBox;
+
+	@FindBy(id="pmg-renewal-upload")
+	static WebElement uploadElement;
+
+	@FindBy(id="address")
+	static WebElement billingAddressBox;
+
+	@FindBy(id="city")
+	static WebElement cityBox;
+
+	@FindBy(xpath="//a[@class='sbSelector' and contains(text(),'Select State')]")
+	static WebElement stateBox;    
+
+	@FindBy(xpath="//a[contains(text(),'Alabama')]")
+	static WebElement state; 
+
+	@FindBy(id="zip")
+	static WebElement zipBox;
+
+	@FindBy(xpath="//a[@class='sbSelector'and contains(text(),'United States')]")
+	static WebElement countryBox; 
+
+	@FindBy(xpath="//a[contains(text(),'United States')]")
+	static WebElement country; 
+
+	@FindBy(id="phone")
+	static WebElement phoneBox;
+
+	@FindBy(xpath="//*[@id='pay_button'and @class='btn btn-primary btn-lg btn-default payment-button']")
+	static WebElement payButton;
+	
+	@FindBy(xpath="//label[contains(text(),'Renewal')][contains(text(),'Form')]")
+	static WebElement renewaFormOption;
 
 	@FindBy(xpath="//span[@class='message notices alert']//ul//li")
 	static WebElement messageNoticeAlert;
@@ -112,37 +149,75 @@ public class PaymentPage {
 		log.info("Pay Link displayed");
 	}
 	
-	@Step("Fill Payment Page Form...")
-	public static void fillPaymentPageForm()
+	@Step("verify Payment page Step...")
+	public static void verifyPaymentPage()
 	{
-		payAmountTextbox.clear();
-		payAmountTextbox.sendKeys(Prop.getTestData("payAmount"));
-		log.info("Pay amount entered");
-		addressTextbox.clear();
-		addressTextbox.sendKeys(Prop.getTestData("billingAddress"));
-		log.info("Address entered");
-		cityTextbox.clear();
-		cityTextbox.sendKeys(Prop.getTestData("billingCity"));
-		log.info("City entered");
-		SeleniumUtils.scrollToBottom();
-		stateTextbox.click();
-		log.info("selecting state");
-		stateList.get(3).click();
-		log.info("state selected");
-		countryDropdown.click();
-		log.info("selecting country");
-		countryList.get(1).click();
-		log.info("country selected");
-		phoneTextbox.clear();
-		phoneTextbox.sendKeys(Prop.getTestData("billingPhone"));
-		log.info("Phone number entered");
+		Assert.assertTrue(paymentElement.isDisplayed());
+		log.info("Payment page is displayed");
 	}
-	
-	@Step("Navigate to payment detail page...")
-	public static void navigateToPaymentDetailPage()
-	{
-		payLink.click();
-		log.info("Clicked on pay link");
+
+	@Step("details of customer to pay any amount greater than zero for renewal..")
+	public static void detailCustomerPayForRenewal(String payAmount,String billingAddress,String city,String zip,String phone){
+		SeleniumUtils.waitForElementToBeVisible(payAmountBox);
+		payAmountBox.clear();
+		payAmountBox.sendKeys(payAmount);
+		log.info("Enter pay Amount ");
+
+
+		uploadElement.click();
+		log.info("Enter upload pdf ");
+
+		RobotUtils.uploadFile(".\\src\\main\\resources\\testFiles\\TestFileForUpload.pdf");
+		log.info("upload pdf from drive ");
+
+		billingAddressBox.clear();
+		billingAddressBox.sendKeys(billingAddress);
+		log.info("Enter billing Address For Renewal of Report ");
+
+		cityBox.clear();
+		cityBox.sendKeys(city);
+		log.info("Enter your city");
+
+		stateBox.click();
+		log.info("choose one state from given states");
+
+		state.click();
+		log.info("Enter your state");
+
+		zipBox.clear();
+		zipBox.sendKeys(zip);
+		log.info("Enter your zip");
+
+
+		countryBox.click();
+		log.info("choose one country from given country");
+
+		country.click();
+		log.info("Enter your country");
+
+		SeleniumUtils.waitForElementToBeClickable(phoneBox);
+		phoneBox.clear();
+		phoneBox.sendKeys(phone);
+		log.info("Enter your phone details");
+
+		SeleniumUtils.waitForElementToBeClickable(payButton);
+		payButton.click();
+		log.info("Enter pay option for payment");
 	}
-	
+
+	@Step("the customer is allowed to upload renewal application...")
+	public static void uploadRenewalApplicationForCustomer() { 
+		uploadElement.click();
+		log.info("Enter upload pdf ");
+
+		RobotUtils.uploadFile(".\\src\\main\\resources\\testFiles\\TestFileForUpload.pdf");
+		log.info("upload pdf from drive ");
+
+	}
+
+	@Step("presence of an additional field 'Renewal Form' on the payment screen for Application Renewal payment")
+	public static void additionalCustomerFieldRenewalForm() {
+		Assert.assertTrue(renewaFormOption.isDisplayed());
+		log.info("renewa Form option displayed");
+	}
 }
