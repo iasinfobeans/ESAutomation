1122cc3acc1afbf526405cd448223b3b22c8f8b6
iasinfobeans <shefali.garg@infobeans.com>
2018-08-02 17:53:47 +0530

    Added three test cases - verifySuccessfulPaymentSubmissionForInvoice, verifyInvoicePayment,verifyNavigationToPaymentGateway

--- ES/src/main/java/com/es/tests/InvoicePayment.java
+++ ES/src/main/java/com/es/tests/InvoicePayment.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import org.testng.annotations.Test;
 import com.es.pom.DashboardPage;
 import com.es.pom.InvoiceListingPage;
+import com.es.pom.PaymentHistoryPage;
+import com.es.pom.CardDetailsPage;
 import com.es.pom.PaymentPage;
 import com.es.pom.SignInPage;
 import com.es.setup.Setup;
@@ -27,4 +29,57 @@ public class InvoicePayment extends Setup {
 			throw e;
 		}
 	}
+	
+	@Test(groups = {"smoke"})
+	@Description("Verify that on clicking the 'Pay' button, the user is navigated to the Payment Gateway")
+	public static void verifyNavigationToPaymentGateway() throws IOException, InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			DashboardPage.navigatingToMyInvoices();
+			InvoiceListingPage.navigateToPaymentPage();
+			PaymentPage.fillPaymentPageForm();
+			PaymentPage.navigateToPaymentGatewayPage();
+			CardDetailsPage.paymentGatewayPageVerification();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyNavigationToPaymentGateway");
+			e.getStackTrace(); 
+			throw e;
+		}
+	}
+	
+	@Test(groups = {"smoke"})
+	@Description("Verify that customer is able to make part payment for an invoice")
+	public static void verifyInvoicePayment() throws IOException, InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			DashboardPage.navigatingToMyInvoices();
+			InvoiceListingPage.navigateToPaymentPage();
+			PaymentPage.fillPaymentPageForm();
+			PaymentPage.navigateToPaymentGatewayPage();
+			CardDetailsPage.customerPayForRenewal(Prop.getTestData("name"), Prop.getTestData("creditCardNumber"), Prop.getTestData("cVCNumber"));
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifyInvoicePayment");
+			e.getStackTrace();
+			throw e;
+		}
+	}
+	
+	@Test(groups = {"smoke"})
+	@Description("Verify that the invoice payment can be submitted successfully with valid Payment details")
+	public static void verifySuccessfulPaymentSubmissionForInvoice() throws IOException, InterruptedException {
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			DashboardPage.navigatingToMyInvoices();
+			InvoiceListingPage.navigateToPaymentPage();
+			PaymentPage.fillPaymentPageForm();
+			PaymentPage.navigateToPaymentGatewayPage();
+			CardDetailsPage.customerPayForRenewal(Prop.getTestData("name"), Prop.getTestData("creditCardNumber"), Prop.getTestData("cVCNumber"));
+			PaymentHistoryPage.verifyPaymentSucess();
+		}catch(Exception e){
+			SeleniumUtils.captureScreenshot("verifySuccessfulPaymentSubmissionForInvoice");
+			e.getStackTrace();
+			throw e;
+		}
+	}
+	
 }
