c94858033cc97c4c3e0191933e5260a8f15f4aa6
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-02 11:09:22 +0530

    added new method - uploadFile

---
+++ ES/src/main/java/com/es/util/RobotUtils.java
@@ -0,0 +1,30 @@
+package com.es.util;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+
+public class RobotUtils {
+
+	public static void uploadFile(String filepath){
+
+		//String str = "C:\\Users\\Priya.Jain\\git\\ESAutomation\\ES\\UploadFile\\Scanned_20180713-1115.pdf";
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Clipboard clipboard = toolkit.getSystemClipboard();
+		StringSelection strSel = new StringSelection(filepath);
+		clipboard.setContents(strSel, null);
+
+		Robot robot = null;
+		try {
+			robot = new Robot();
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		robot.delay(250);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyPress(KeyEvent.VK_ENTER);
+	}
+}
