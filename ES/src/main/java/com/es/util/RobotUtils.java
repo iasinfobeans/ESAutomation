d8c5da8547d5c3cff2a31bb30b4692e2902b8057
Pankaj Shukla <pankajs@surveymonkey.com>
2018-08-02 12:27:09 +0530

    replaced

--- ES/src/main/java/com/es/util/RobotUtils.java
+++ ES/src/main/java/com/es/util/RobotUtils.java
@@ -1,18 +1,37 @@
 package com.es.util;
-
 import java.awt.AWTException;
 import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 
-public class RobotUtils {
-	public static void uploadFile(){
+public class RobotUtils {              
+
+	public static void uploadFile(String filePath){
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		Clipboard clipboard = toolkit.getSystemClipboard();
+		StringSelection strSel = new StringSelection(filePath);
+		clipboard.setContents(strSel, null);
+		Robot robot = null;
 		try {
-			Robot robot = new Robot();
-			robot.keyPress(KeyEvent.VK_0);
+			robot = new Robot();
 		} catch (AWTException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+		robot.delay(250);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyPress(KeyEvent.VK_ENTER);
 	}
 }
+
+
+
+
+
+
+
+
+
+
