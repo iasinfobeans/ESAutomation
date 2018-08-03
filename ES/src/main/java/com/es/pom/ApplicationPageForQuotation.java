29b127438eb89a31d723905bb670a0cbc61b2543
Pankaj Shukla <pankajs@surveymonkey.com>
2018-08-03 12:43:30 +0530

    Added submitApplication, fillContactDetails, agreementCheckBox, submitAgreement, couponOption

--- ES/src/main/java/com/es/pom/ApplicationPageForQuotation.java
+++ ES/src/main/java/com/es/pom/ApplicationPageForQuotation.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 
+import com.es.setup.Setup;
+import com.es.util.SeleniumUtils;
+
 import io.qameta.allure.Step;
 
 public class ApplicationPageForQuotation {
@@ -45,8 +48,55 @@ public class ApplicationPageForQuotation {
 
 	@FindBy(xpath="//span[@class='message success alert']")
 	static WebElement successAlert;
+	
+	@FindBy(xpath="//*[@id='tech_rep_name']")
+	static WebElement technicalRepresentativeName;
+	
+	@FindBy(xpath="//*[@id='auth_sign_name']")
+	static WebElement authSignatoryName;
+	
+	@FindBy(xpath="//*[@id='billing_rep_name']")
+	static WebElement billingRepresentativeName;
+	
+	@FindBy(xpath="//*[@id='same_as_company_technical']")
+	static WebElement sameAsCompanyAddressCheckbox;
+	
+	@FindBy(xpath="//*[@id='same_as_company_authorized']")
+	static WebElement sameAsAuthSignAddressCheckbox;
+	
+	@FindBy(xpath="//*[@id='same_as_company_billing']")
+	static WebElement sameAsBillingAddressCheckbox;
+	
+	@FindBy(xpath="//*[@id='tech_rep_email']")
+	static WebElement technicalRepresentativeEmail;
+	
+	@FindBy(xpath="//*[@id='auth_rep_email']")
+	static WebElement authSignatoryEmail;
+	
+	@FindBy(xpath="//*[@id='billing_rep_email']")
+	static WebElement billingRepresentativeEmail;
 
+	@FindBy(xpath="//*[@id='app_sidebar_submit']")
+	static WebElement submitApplication;
+	
+	@FindBy(xpath="//h1[text()='Disclaimer']")
+	static WebElement submitPopup;
 
+	@FindBy(xpath="//div[@id='submit_popup']//ul[@class='radio-frm agreement-checkbox']")
+	static WebElement submitAgreement;
+	
+	@FindBy(xpath="//*[@id='continue_to_submit']")
+	static WebElement continueToSubmit;
+	
+	@FindBy(linkText = "Pay")
+	static WebElement pay;
+	
+	@FindBy(xpath="//*[@class='custom-input-box basic-fees-phone']")
+	static WebElement basicFees;
+	
+	@FindBy(xpath="//*[@id='other_amount']")
+	static WebElement quotationAmount;
+	
 	@Step("verify Application page for View Quote...")
 	public static void verifyApplicationPageForViewQuote() {
 		Assert.assertTrue(applicationElement.isDisplayed());
@@ -103,5 +153,85 @@ public class ApplicationPageForQuotation {
 	{
         Assert.assertTrue(successAlert.isDisplayed());
 		log.info("success Alert is displayed");
+	}
+	
+	@Step("Clicking on submit button...")
+	public static void submitApplication() {
+		SeleniumUtils.waitForElementToBeVisible(submitApplication);
+		saveButton.click();
+		log.info("Click on save Button");
+		submitApplication.click();
+		log.info("Clicked on Submit Button");
+	}
+	
+	@Step("Entering contact information...")
+	public static void fillContactDetails(String techRepName, String authRepName, String billingRepName,
+			String techRepEmail, String authRepEmail, String billingRepEmail) {
+		technicalRepresentativeName.clear();
+		technicalRepresentativeName.sendKeys(techRepName);
+		authSignatoryName.clear();
+		authSignatoryName.sendKeys(authRepName);
+		billingRepresentativeName.clear();
+		billingRepresentativeName.sendKeys(billingRepName);
+		sameAsCompanyAddressCheckbox.click();
+		sameAsAuthSignAddressCheckbox.click();
+		sameAsBillingAddressCheckbox.click();
+		technicalRepresentativeEmail.clear();
+		technicalRepresentativeEmail.sendKeys(techRepEmail);
+		authSignatoryEmail.clear();
+		authSignatoryEmail.sendKeys(authRepEmail);
+		billingRepresentativeEmail.clear();
+		billingRepresentativeEmail.sendKeys(billingRepEmail);
+		
+		log.info("Entered contact information");
+	}
+
+	@Step("Accepting agreement terms and conditions...")
+	public static void agreementCheckBox() {
+		SeleniumUtils.waitForElementToBeVisible(submitPopup);
+		Assert.assertTrue(submitPopup.isDisplayed());
+		log.info("submit pop up displayed.");
+		
+		
+		SeleniumUtils.waitForElementToBeVisible(submitAgreement);
+		submitAgreement.click();
+		log.info("Accept Agreement terms and condition.");
+	}
+	
+	@Step("Submitting agreement...")
+	public static void submitAgreement() {
+		SeleniumUtils.waitForElementToBeVisible(continueToSubmit);
+		continueToSubmit.click();
+		log.info("Submit Agreement.");
+	}
+
+	@Step("clicking on pay option...")
+	public static void pay() {
+		Assert.assertTrue(pay.isDisplayed());
+		pay.click();
+		log.info("Clicked on Pay option.");
+	}
+
+	@Step("Observing basic fees and Quotation amount...")
+	public static void observFees() {
+		
+		String basicFee = basicFees.getAttribute("Value");
+		basicFee = basicFee.substring(0, basicFee.indexOf("."));
+		String quotAmount = quotationAmount.getAttribute("Value");
+		quotAmount = quotAmount.substring(0, quotAmount.indexOf("."));
+		
+		if(Integer.parseInt(basicFee) > Integer.parseInt(quotAmount)) {
+			log.info("Basic fees is sufficient.");
+		}
+		log.info("Observed basic fees and Quotation amount.");
+	}
+	
+	@Step("Searching for coupon option for application which is applied through Quotation...")
+	public static void couponOption() {
+		if (Setup.driver.getPageSource().contains("Do you have a coupon?")) {
+			log.info("Coupon Discount available for Quotation.");
+		}else {
+			log.info("Coupon Discount is not available for Quotation.");
+		}
 	}
 }
