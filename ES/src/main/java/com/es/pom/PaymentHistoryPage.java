b03ef23d11d8304873a9693d0ea952e8a8e89fce
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-06 12:12:14 +0530

    Added verifyNewApplicationCreated method

--- ES/src/main/java/com/es/pom/PaymentHistoryPage.java
+++ ES/src/main/java/com/es/pom/PaymentHistoryPage.java
@@ -3,7 +3,9 @@ package com.es.pom;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
 import com.es.util.Prop;
+import com.es.util.SeleniumUtils;
 import io.qameta.allure.Step;
 
 public class PaymentHistoryPage {
@@ -12,10 +14,21 @@ public class PaymentHistoryPage {
 	@FindBy(xpath="//span[@class='message success alert']//ul//li")
 	static WebElement paymentSuccessMessege;
 
+	@FindBy(xpath="//*[@id='paymentListTable']/tbody/tr[1]/td[5]")
+	static WebElement newApplication;
+
 	@Step("Verify that the payment was successful.")
 	public static void verifyPaymentSucess(){
 		if(paymentSuccessMessege.getText().equals(Prop.getTestData("paymentSuccessMessage"))){
 			log.info("Payment was successful");
 		}
+	}
+
+	@Step("A new application should be created on the portal of the same program type....")
+	public static void verifyNewApplicationCreated()
+	{
+		SeleniumUtils.waitForElementToBeClickable(newApplication);
+		Assert.assertTrue(newApplication.isDisplayed());
+		log.info("New Application is displayed");
 	}
 }
