f48f17bbf1261216adeacbf9afe569bc1a010c93
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-07 18:05:31 +0530

    Added new method - verifyNewApplicationCreated and modified xpath for newApplication

--- ES/src/main/java/com/es/pom/PaymentHistoryPage.java
+++ ES/src/main/java/com/es/pom/PaymentHistoryPage.java
@@ -13,6 +13,8 @@ public class PaymentHistoryPage {
 	static WebElement paymentSuccessMessege;
 
 	@FindBy(xpath="//*[@id='paymentListTable']/tbody/tr[1]/td[5]")
+	//@FindBy(xpath="//*[@id='paymentListTable']//tr[./td[.='iasinfobeans@yopmail.com']][./td[.='Report']]/td[.='PMG-1004']\")")
+	//div[@class='col-md-6 col-xs-6 padding-left-0 pay_details' ]//label/following-sibling:://div[@class='col-md-6 col-xs-6 padding-right-0  pay_details  pull-right']
 	static WebElement newApplication;
 
 	@Step("Verify that the payment was successful.")
@@ -20,5 +22,13 @@ public class PaymentHistoryPage {
 		if(paymentSuccessMessege.getText().equals(Prop.getTestData("paymentSuccessMessage"))){
 			log.info("Payment was successful");
 		}
+	}
+
+	@Step("A new application should be created on the portal of the same program type....")
+	public static void verifyNewApplicationCreated()
+	{
+		SeleniumUtils.waitForElementToBeClickable(newApplication);
+		Assert.assertTrue(newApplication.isDisplayed());
+		log.info("New Application is displayed");
 	}
 }
