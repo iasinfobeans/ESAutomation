3e5a31ef6760a09ac02dced3c795c35993563f5b
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-10 16:24:36 +0530

    Added ReportRenewalWorkflow group

--- ES/src/main/java/com/es/tests/ReportRenewalWorkflow.java
+++ ES/src/main/java/com/es/tests/ReportRenewalWorkflow.java
@@ -18,7 +18,7 @@ import io.qameta.allure.Description;
 
 public class ReportRenewalWorkflow extends Setup{
 
-	@Test(groups = { "smoke" })
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })
 	@Description("Verify the customer Dashboard to have an additional head titled 'Reports (Pending Renewal.")
 	public void verifyGetAQuoteButtonOnQuotationListingPage() throws InterruptedException {
 		try{
@@ -32,7 +32,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" }) 
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })
 	@Description("Verify the actions available on hovering over any report.")
 	public void verifyactionsAvailableHoveringOverAnyReport() throws InterruptedException {
 		try{
@@ -47,7 +47,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify the action on clicking the 'Pay' button")
 	public void verifyActionOnPayButton() throws InterruptedException {
 		try{
@@ -55,7 +55,7 @@ public class ReportRenewalWorkflow extends Setup{
 			OverlayPage.skipoverlayPage();
 			DashboardPage.hoveringOverAnyReport();
 			DashboardPage.clickActionOnPayButton();
-			PaymentPage.verifyPaymentPageElements();
+			PaymentPage.verifyPaymentPageElementsForReportRenewal();
 		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyActionOnPayButton");
 			e.getStackTrace();
@@ -63,9 +63,9 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })  
 	@Description("Verify the action on clicking the 'View Projects' button")
-	public void verifyActionOnViewInvoicessButton() throws InterruptedException {
+	public void verifyActionOnViewProjectsButton() throws InterruptedException {
 		try{
 			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
 			OverlayPage.skipoverlayPage();
@@ -79,7 +79,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })  
 	@Description("Verify the action on clicking the 'View Invoices' button")
 	public void verifyActionOnViewInvoicesButton() throws InterruptedException {
 		try{
@@ -95,7 +95,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that the Technical contact associated with the report can see the report at the Dashboard under the head 'Reports (Pending Approval)")
 	public void verifyTechnicalContactAssociatedWithReport() throws InterruptedException {
 		try{
@@ -109,7 +109,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that the Additional Technical contact associated with the report can see the report at the Dashboard under the head 'Reports (Pending Approval)' ")
 	public void verifyAdditionalTechnicalContactAssociatedWithReport() throws InterruptedException {
 		try{
@@ -123,7 +123,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that the Authorized signatory associated with the report can see the report at the Dashboard under the head 'Reports (Pending Approval)")
 	public void verifyAuthorizedSignatoryAssociatedWithReport() throws InterruptedException {
 		try{
@@ -137,7 +137,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })    
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that the payment screen allows the customer to pay any amount greater than zero for renewal.")
 	public void verifyCustomerDetailsPayForRenewal()  throws InterruptedException {
 		try{
@@ -153,7 +153,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that for renewal, the customer is allowed to upload renewal application.")
 	public void verifyCustomerAllowedUploadRenewalApplication()  throws InterruptedException {
 		try{
@@ -170,7 +170,7 @@ public class ReportRenewalWorkflow extends Setup{
 	}
 
 
-	@Test(groups = { "smoke" })    
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })  
 	@Description("Verify the presence of an additional field 'Renewal Form' on the payment screen for Application Renewal payment.")
 	public void verifyCustomerFieldRenewalForm()  throws InterruptedException {
 		try{	
@@ -187,7 +187,7 @@ public class ReportRenewalWorkflow extends Setup{
 
 	}
 
-	@Test(groups = { "smoke" })   
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that the Renewal Form field is a mandatory field for PMG renewal")
 	public void verifyRenewalFormMandatoryFieldPMGRenewal()  throws InterruptedException {
 		try{
@@ -204,7 +204,7 @@ public class ReportRenewalWorkflow extends Setup{
 	}
 
 
-	@Test(groups = { "smoke" })   
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })  
 	@Description("Verify that the Renewal Form field is not a mandatory field for ESL & ESR renewal")
 	public void verifyRenewalFormNotMandatoryRenewalEslEsr() throws InterruptedException {
 		try{
@@ -220,7 +220,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })   
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })
 	@Description("Verify that user is able to pay renewal fee against report no.")
 	public void verifyRenewalFeesAgainstReportNo()  throws InterruptedException {
 		try{
@@ -237,7 +237,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })   
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })  
 	@Description("Verify that the Renewal payment can be submitted successfully with valid Payment details.")
 	public void verifyRenewalPaymentSubmitted()  throws InterruptedException {
 		try{
@@ -254,7 +254,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })   
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })  
 	@Description("Verify the mail received by customer for Payment.")
 	public void verifyMailReceivedByCustomerForPayment()  throws InterruptedException {
 		try{
@@ -273,7 +273,7 @@ public class ReportRenewalWorkflow extends Setup{
 	}
 
 
-	@Test(groups = { "smoke" })   
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify the mail received to associated staff for Payment.")
 	public void verifyMailReceivedByStaffForPayment()  throws InterruptedException {
 		try{
@@ -291,7 +291,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that on successful renewal payment for any application, a new application is created on the portal of the same program type.")
 	public void verifyNewApplicationOnPortal()  throws InterruptedException {
 		try{
@@ -300,6 +300,7 @@ public class ReportRenewalWorkflow extends Setup{
 			DashboardPage.hoveringOverAnyReport();
 			DashboardPage.clickActionOnPayButton();
 			PaymentPage.detailCustomerPayForRenewal(Prop.getTestData("payAmount"),Prop.getTestData("billingAddress"),Prop.getTestData("city"),Prop.getTestData("zip"),Prop.getTestData("phone"));
+			CardDetailsPage.verifyOderIdNewApplicationCreated();
 			CardDetailsPage.customerPayForRenewal(Prop.getTestData("name"), Prop.getTestData("creditCardNumber"), Prop.getTestData("cVCNumber"));
 			PaymentHistoryPage.verifyNewApplicationCreated();
 		}catch(Exception e){
@@ -309,7 +310,7 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow", })
 	@Description("Verify that on newly created applications, user should not be able to perform predefined operations like edit/view/delete/recall")
 	public void verifyUserUnablePerformPredefinedOperations()  throws InterruptedException {
 		try{
@@ -319,9 +320,8 @@ public class ReportRenewalWorkflow extends Setup{
 			DashboardPage.clickActionOnPayButton();
 			PaymentPage.detailCustomerPayForRenewal(Prop.getTestData("payAmount"),Prop.getTestData("billingAddress"),Prop.getTestData("city"),Prop.getTestData("zip"),Prop.getTestData("phone"));
 			CardDetailsPage.customerPayForRenewal(Prop.getTestData("name"), Prop.getTestData("creditCardNumber"), Prop.getTestData("cVCNumber"));
-			PaymentHistoryPage.verifyNewApplicationCreated();
 			DashboardPage.verifyDashboardPage();
-			DashboardPage.navigateToApplicationPage();
+			DashboardPage.navigateToApplicationPageWhenLoggedIn();
 			ApplicationsListingPage.verifyUserUnablePerformPredefinedOperations();
 		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyUserUnablePerformPredefinedOperations");
@@ -330,14 +330,15 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that on newly created applications, user should not be able to perform predefined operations like edit/view/delete/recall")
 	public void verifyStaffUserUnablePerformPredefinedOperations()  throws InterruptedException {
 
-		try{	SignInPage.login(Prop.getTestData("Staffuser"),Prop.getTestData("Staffpassword"), "Staff");
-		DashboardPage.verifyDashboardPage();
-		DashboardPage.navigateToApplicationPage();
-		ApplicationsListingPage.verifyUserUnablePerformPredefinedOperations();
+		try{	
+			SignInPage.login(Prop.getTestData("Staffuser"),Prop.getTestData("Staffpassword"), "Staff");
+			DashboardPage.verifyDashboardPage();
+			DashboardPage.navigateToApplicationPageWhenLoggedIn();
+			ApplicationsListingPage.verifyUserUnablePerformPredefinedOperations();
 		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyStaffUserUnablePerformPredefinedOperations");
 			e.getStackTrace();
@@ -347,7 +348,7 @@ public class ReportRenewalWorkflow extends Setup{
 	}
 
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })
 	@Description("Verify that the Customer is able to download Renewal Application document from application listing")
 	public void verifyCustomerDownloadRenewalApplicationDocument()  throws InterruptedException {
 		try{
@@ -357,9 +358,8 @@ public class ReportRenewalWorkflow extends Setup{
 			DashboardPage.clickActionOnPayButton();
 			PaymentPage.detailCustomerPayForRenewal(Prop.getTestData("payAmount"),Prop.getTestData("billingAddress"),Prop.getTestData("city"),Prop.getTestData("zip"),Prop.getTestData("phone"));
 			CardDetailsPage.customerPayForRenewal(Prop.getTestData("name"), Prop.getTestData("creditCardNumber"), Prop.getTestData("cVCNumber"));
-			PaymentHistoryPage.verifyNewApplicationCreated();
 			DashboardPage.verifyDashboardPage();
-			DashboardPage.navigateToApplicationPage();
+			DashboardPage.navigateToApplicationPageWhenLoggedIn();
 			ApplicationsListingPage.customerDownloadRenewalApplicationDocument();
 		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyCustomerDownloadRenewalApplicationDocument");
@@ -368,13 +368,13 @@ public class ReportRenewalWorkflow extends Setup{
 		}
 	}
 
-	@Test(groups = { "smoke" })  
+	@Test(groups = { "smoke","ReportRenewalWorkflow" }) 
 	@Description("Verify that the Staff is able to download Renewal Application document from application listing.")
 	public void verifyStaffDownloadRenewalApplicationDocument()  throws InterruptedException {
 		try{
 			SignInPage.login(Prop.getTestData("Staffuser"),Prop.getTestData("Staffpassword"), "Staff");
 			DashboardPage.verifyDashboardPage();
-			DashboardPage.navigateToApplicationPage();
+			DashboardPage.navigateToApplicationPageWhenLoggedIn();
 			ApplicationsListingPage.customerDownloadRenewalApplicationDocument();
 		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyStaffDownloadRenewalApplicationDocument");
@@ -384,7 +384,7 @@ public class ReportRenewalWorkflow extends Setup{
 	}
 
 
-	@Test(groups = { "smoke" })    
+	@Test(groups = { "smoke","ReportRenewalWorkflow" })   
 	@Description("Verify that after successful payment, staff is notified about new renewal file available.")
 	public void verifyStaffNotifiedAboutNewRenewalFile()  throws InterruptedException {
 		try{
