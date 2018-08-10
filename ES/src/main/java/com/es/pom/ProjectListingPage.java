a1a5f1f409071cbb19d295d4faf67f631f7b4fea
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-10 16:13:05 +0530

    Added new method - navigateToProjectListingPage

--- ES/src/main/java/com/es/pom/ProjectListingPage.java
+++ ES/src/main/java/com/es/pom/ProjectListingPage.java
@@ -6,16 +6,25 @@ import org.testng.Assert;
 import io.qameta.allure.Step;
 
 public class ProjectListingPage {
-	
+
 	private static Logger log = Logger.getLogger(ProjectListingPage.class.getName());
 
 	@FindBy(xpath="//div[@class='ehading']")
 	static WebElement projectsElement;
+
+	@FindBy(xpath="//*[@id='projectTable']//a[contains(text(),'View SOW')]")
+	static WebElement viewSOWOption;
 
 	@Step("Dashboard to have an additional head titled 'Reports.")
 	public static void verifyViewProjectsPage(){
 		Assert.assertTrue(projectsElement.isDisplayed());
 		log.info("Reports is displayed on Dashboard");
 	}	
+
+	@Step("Click on view SOW to be Download")
+	public static void navigateToProjectListingPage() {
+		viewSOWOption.click();
+		log.info("View SOW to be Download");
+	}
 
 }
