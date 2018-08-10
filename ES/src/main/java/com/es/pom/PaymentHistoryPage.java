b15147068341478a17d5b29dc716c4e3918a7658
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-10 16:10:42 +0530

    Updated verifyNewApplicationCreated method

--- ES/src/main/java/com/es/pom/PaymentHistoryPage.java
+++ ES/src/main/java/com/es/pom/PaymentHistoryPage.java
@@ -4,10 +4,8 @@ import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
-
 import com.es.util.Prop;
 import com.es.util.SeleniumUtils;
-
 import io.qameta.allure.Step;
 
 public class PaymentHistoryPage {
@@ -16,8 +14,11 @@ public class PaymentHistoryPage {
 	@FindBy(xpath="//span[@class='message success alert']//ul//li")
 	static WebElement paymentSuccessMessege;
 
-	@FindBy(xpath="//span[@class='pull-right']/parent::div/preceding-sibling::div//label[text()='Order ID:']/../following-sibling::div/span")
-	static WebElement newApplication;
+	@FindBy(xpath="//*[@id='paymentListTable_filter']//label//input[@class='form-control input-sm']")
+	static WebElement searchTextbox;
+
+	@FindBy(xpath="//*[contains(text(),'Credit Card')]")
+	static WebElement paymentMode;
 
 	@Step("Verify that the payment was successful.")
 	public static void verifyPaymentSucess(){
@@ -27,10 +28,12 @@ public class PaymentHistoryPage {
 	}
 
 	@Step("A new application should be created on the portal of the same program type....")
-	public static void verifyNewApplicationCreated()
-	{
-		SeleniumUtils.waitForElementToBeClickable(newApplication);
-		Assert.assertTrue(newApplication.isDisplayed());
-		log.info("New Application is displayed");
-	}
+	public static void verifyNewApplicationCreated() {
+		String orderId = CardDetailsPage.verifyOderIdNewApplicationCreated();
+		searchTextbox.sendKeys(orderId);
+		log.info("Enter the OderId in the Search Box");
+
+		Assert.assertTrue(paymentMode.isDisplayed());
+		log.info("payment Mode for application is displayed");
+  }
 }
