9c4d703b8fc914b844932780809e5d6545eca0dc
Nisha Gathiya <nisha.gathiya@infobeans.com>
2018-08-20 14:19:26 +0530

    Changed method name - navigateToApplicationListingPageWhenLoggedIn

--- ES/src/main/java/com/es/tests/DeleteApplication.java
+++ ES/src/main/java/com/es/tests/DeleteApplication.java
@@ -1,9 +1,7 @@
 package com.es.tests;
 
 import java.io.IOException;
-
 import org.testng.annotations.Test;
-
 import com.es.pom.ApplicationPage;
 import com.es.pom.DashboardPage;
 import com.es.pom.PMGApplicationFormPage;
@@ -11,42 +9,40 @@ import com.es.pom.SignInPage;
 import com.es.setup.Setup;
 import com.es.util.Prop;
 import com.es.util.SeleniumUtils;
-
 import io.qameta.allure.Description;
 
 public class DeleteApplication extends Setup {
 
-	@Test(groups = { "smoke", "DeleteApplication" })
+	@Test(groups = {"smoke","DeleteApplication"})
 	@Description("Verify the Delete Application functionality for Unapproved user.")
 	public static void verifyApplicationDelete_UnapprovedUser() throws IOException, InterruptedException {
-		try {
-			SignInPage.login(Prop.getTestData("unApprovedUser"), Prop.getTestData("unApprovedUserPassword"),
-					"Customer");
+		try{
+			SignInPage.login(Prop.getTestData("unApprovedUser"),Prop.getTestData("unApprovedUserPassword"), "Customer");
 			DashboardPage.navigateToPMGApplicationProgram();
 			PMGApplicationFormPage.PmgApplicationSave();
-			DashboardPage.navigateToApplicationPageWhenLoggedIn();
+			DashboardPage.navigateToApplicationListingPageWhenLoggedIn();
 			ApplicationPage.deleteApplication();
-		} catch (Exception e) {
+		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyApplicationDelete_UnapprovedUser");
 			e.getStackTrace();
 			throw e;
 		}
 	}
-
-	@Test(groups = { "smoke", "DeleteApplication" })
+	
+	@Test(groups = {"smoke","DeleteApplication"})
 	@Description("Verify the Delete Application functionality for Aapproved user.")
 	public static void verifyApplicationDelete_AprovedUser() throws IOException, InterruptedException {
-		try {
-			SignInPage.login(Prop.getTestData("username"), Prop.getTestData("password"), "Customer");
+		try{
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
 			DashboardPage.navigateToPMGApplicationProgram();
 			PMGApplicationFormPage.PmgApplicationSave();
-			DashboardPage.navigateToApplicationPageWhenLoggedIn();
+			DashboardPage.navigateToApplicationListingPageWhenLoggedIn();
 			ApplicationPage.deleteApplication();
-		} catch (Exception e) {
+		}catch(Exception e){
 			SeleniumUtils.captureScreenshot("verifyApplicationDelete_AprovedUser");
 			e.getStackTrace();
 			throw e;
 		}
 	}
-
+	
 }
