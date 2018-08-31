659ae75bf537f4c1d30e504aa88390ee34f474d3
Nisha Gathiya <nisha.gathiya@infobeans.com>
2018-08-31 14:27:47 +0530

    Refactored all the methods

--- ES/src/main/java/com/es/tests/ApplicationEditForStaff.java
+++ ES/src/main/java/com/es/tests/ApplicationEditForStaff.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import com.es.pom.ApplicationPage;
 import com.es.pom.ApplicationsListingPage;
 import com.es.pom.DashboardPage;
+import com.es.pom.PMGApplicationFormPage;
 import com.es.pom.SignInPage;
 import com.es.setup.Setup;
 import com.es.util.Prop;
@@ -17,6 +18,12 @@ public class ApplicationEditForStaff extends Setup {
 	@Description("Verify that the staff has an option to edit the Applications that are in 'Pending Review' status")
 	public static void verifyEditOptionForPendingReview() throws IOException, InterruptedException {
 		try {
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			DashboardPage.navigateToPMGApplicationProgramApprovedUser();
+			PMGApplicationFormPage.PmgApplicationFormFill(Prop.getTestData("username"));
+			PMGApplicationFormPage.SignAsAuthorizedSignatory();
+			PMGApplicationFormPage.PmgApplicationFormSubmit();
+			DashboardPage.logout();
 			SignInPage.login(Prop.getTestData("Staffuser"), Prop.getTestData("Staffpassword"), "Staff");
 			DashboardPage.navigateToApplicationListingPage();
 			ApplicationsListingPage.checkEditOptionInPendingReviewApp();
@@ -31,10 +38,16 @@ public class ApplicationEditForStaff extends Setup {
 	@Description("Verify that the staff is able to edit all the sections except for the signatory information")
 	public static void verifyEditOptionForSignatoryInformation() throws IOException, InterruptedException {
 		try {
+			SignInPage.login(Prop.getTestData("username"),Prop.getTestData("password"), "Customer");
+			DashboardPage.navigateToPMGApplicationProgramApprovedUser();
+			PMGApplicationFormPage.PmgApplicationFormFill(Prop.getTestData("username"));
+			PMGApplicationFormPage.SignAsAuthorizedSignatory();
+			PMGApplicationFormPage.PmgApplicationFormSubmit();
+			DashboardPage.logout();
 			SignInPage.login(Prop.getTestData("Staffuser"), Prop.getTestData("Staffpassword"), "Staff");
 			DashboardPage.navigateToApplicationListingPage();
-			//String appId = ApplicationsListingPage.getFirstApplicationId();
-			//ApplicationsListingPage.navigateToEditApplicationPage(appId);
+			String appId = ApplicationsListingPage.getFirstApplicationId();
+			ApplicationsListingPage.navigateToEditApplicationPage(appId);
 			ApplicationPage.verifySignatoryInfoIsNotEditable();
 		} catch (Exception e) {
 			SeleniumUtils.captureScreenshot("verifyEditOptionForSignatoryInformation");
