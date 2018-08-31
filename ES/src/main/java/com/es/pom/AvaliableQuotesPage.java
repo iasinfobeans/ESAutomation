45dd555b914e1a9b30c67e0a6990f5fb8aacfd8e
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-31 18:04:56 +0530

    Added two new methods - viewQuotesForExpiredQuote and verifyAppliedQuote

--- ES/src/main/java/com/es/pom/AvaliableQuotesPage.java
+++ ES/src/main/java/com/es/pom/AvaliableQuotesPage.java
@@ -3,6 +3,9 @@ package com.es.pom;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.testng.Assert;
+
+import com.es.util.SeleniumUtils;
 
 import io.qameta.allure.Step;
 
@@ -10,18 +13,66 @@ public class AvaliableQuotesPage {
 
 	private static Logger log = Logger.getLogger(AvaliableQuotesPage.class.getName());
 
-	@FindBy(xpath = "//*[@programslug='esr']")
+	@FindBy(xpath = "//*[@class='sbHolder']//a[contains(text(),'All Status')]")
+	static WebElement statusAvailableQuotes;
+
+	@FindBy(xpath = "//a[contains(text(),'Apply')]")
 	static WebElement applyButton;
+
+	@FindBy(xpath = "//*[@class='sbHolder']//a[contains(text(),'Applied')]")
+	static WebElement appliedQuotes;
+
+	@FindBy(xpath = "//strong[text()='Applied']")
+	static WebElement appliedQuoteStatus;
+
+	@FindBy(linkText = "Edit")
+	static WebElement edit;
+
+	@FindBy(xpath ="//*[@id='view-quotes-dataTable']//a[@class='tbl-btn'][contains(text(),'View')]")
+	static WebElement view;
 
 	/**
 	 * This method will verify customer Click on the Apply button .
 	 * @return void
 	 * @param void
 	 */
-	@Step(" Click on the Apply button ...")
+	@Step("Click on the Apply button")
 	public static void applyForViewQuotesOptions() {
+		SeleniumUtils.waitForElementToBeVisible(applyButton);
+		SeleniumUtils.waitForElementToBeClickable(applyButton);
 		applyButton.click();
+		Assert.assertTrue(applyButton.isDisplayed());
 		log.info("Verify Customer is able to Click on Apply button ");
+
+	}
+
+	/**
+	 * This method will verify quote should not be allowed to be used for submitting another application.
+	 * @return void
+	 * @param void
+	 */
+	@Step("Verify quote not allowed to be used to submitt another application")
+	public static void verifyAppliedQuote() {
+		Assert.assertTrue(appliedQuoteStatus.isDisplayed());
+		Assert.assertTrue(edit.isDisplayed());
+		log.info("Verify Customer is able to edit on Applied Quote but cannot aplly again.");
+
+	}
+
+	/**
+	 * This method will verify customer Click on the view button .
+	 * @return void
+	 * @param void
+	 */
+	@Step("Click on the view button")
+	public static void viewQuotesForExpiredQuote() {
+		SeleniumUtils.waitForElementToBeVisible(view);
+		SeleniumUtils.waitForElementToBeClickable(view);
+		Assert.assertTrue(view.isDisplayed());
+		view.click();
+		SeleniumUtils.executeJavaScript("arguments[0].click();", view);
+		SeleniumUtils.executeJavaScript("arguments[0].click();", view);
+		log.info("Verify Customer is able to Click on view button ");
 
 	}
 }
