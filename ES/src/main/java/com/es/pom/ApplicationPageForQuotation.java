2828a0d34a1e9d4ebaa1aba7754b6c85e6d13858
Priya.Jain <Priya.Jain@IB-SEZ-PC-490.infobeans.in>
2018-08-31 17:59:35 +0530

    Added payForExpiredQuote method and refactored few

--- ES/src/main/java/com/es/pom/ApplicationPageForQuotation.java
+++ ES/src/main/java/com/es/pom/ApplicationPageForQuotation.java
@@ -97,14 +97,35 @@ public class ApplicationPageForQuotation {
 	@FindBy(xpath = "//*[@id='other_amount']")
 	static WebElement quotationAmount;
 
-	@Step("verify Application page for View Quote...")
+    @FindBy(xpath = "//*[@id='app_sidebar_pay_btn']")
+	static WebElement payNow;
+	
+	/**
+	 * This method will verify Application page for View Quote.
+	 * @return void
+	 * @param void
+	 */
+   @Step("Verify Application page for View Quote")
 	public static void verifyApplicationPageForViewQuote() {
 		Assert.assertTrue(applicationElement.isDisplayed());
-		log.info("Verify Register from PMG displayed");
+		log.info("Application page for View Quote");
 
 	}
+	/**
+	 * This method will Enter information exactly as it should appear in published report.
+	 * @param companyName
+	 * @param companyLegalStatus
+	 * @param mailingAddress
+	 * @param city
+	 * @param state
+	 * @param  zip
+	 * @param companyPhoneNumber
+	 * @param companyEmailAddress
+	 * @param subjectOfReport
+	 * @return void
+	 */
 
-	@Step("Enter information exactly as it should appear in published report...")
+	@Step("Enter information for report")
 	public static void fillApplication(String companyName, String companyLegalStatus, String mailingAddress,
 			String city, String state, String zip, String companyPhoneNumber, String companyEmailAddress,
 			String subjectOfReport) {
@@ -146,11 +167,16 @@ public class ApplicationPageForQuotation {
 		subjectOfReportTextBox.sendKeys(subjectOfReport);
 		log.info("Enter your subject Of Report");
 
+		Assert.assertTrue(saveButton.isDisplayed());
 		saveButton.click();
 		log.info("Click on save Button");
 	}
+	
+	/**
+	 * This method will click on save and submit application.
+	 */
 
-	@Step("Verify successful submission of information to be published in report...")
+	@Step("Verify successful submission of information to be published in report")
 	public static void verifySuccessfulSubmissionOfreport() {
 		Assert.assertTrue(successAlert.isDisplayed());
 		log.info("success Alert is displayed");
@@ -158,8 +184,10 @@ public class ApplicationPageForQuotation {
 
 	/**
 	 * This method will click on save and submit application.
+	 * @param void
+	 * @return void
 	 */
-	@Step("Clicking on submit button...")
+	@Step("Click on submit button")
 	public static void submitApplication() {
 		SeleniumUtils.waitForElementToBeVisible(submitApplication);
 		Assert.assertTrue(submitApplication.isDisplayed());
@@ -171,7 +199,6 @@ public class ApplicationPageForQuotation {
 
 	/**
 	 * This method will fill the contact details while filling the application.
-	 * 
 	 * @param techRepName
 	 * @param authRepName
 	 * @param billingRepName
@@ -180,7 +207,7 @@ public class ApplicationPageForQuotation {
 	 * @param billingRepEmail
 	 * @return void
 	 */
-	@Step("Entering contact information...")
+	@Step("Enter contact information")
 	public static void fillContactDetails(String techRepName, String authRepName, String billingRepName,
 			String techRepEmail, String authRepEmail, String billingRepEmail) {
 		Assert.assertTrue(technicalRepresentativeName.isDisplayed());
@@ -205,8 +232,11 @@ public class ApplicationPageForQuotation {
 
 	/**
 	 * This method will accept agreement terms and conditions.
+	 * @param billingRepEmail
+	 * @return void
+	 * @param void
 	 */
-	@Step("Accepting agreement terms and conditions...")
+	@Step("Accept agreement terms and conditions")
 	public static void agreementCheckBox() {
 		SeleniumUtils.waitForElementToBeVisible(submitPopup);
 		Assert.assertTrue(submitPopup.isDisplayed());
@@ -219,19 +249,22 @@ public class ApplicationPageForQuotation {
 
 	/**
 	 * This method will submit agreement.
+	 * @return void
+	 * @param void
 	 */
-	@Step("Submitting agreement...")
+	@Step("Submit agreement")
 	public static void submitAgreement() {
 		SeleniumUtils.waitForElementToBeVisible(continueToSubmit);
-		Assert.assertTrue(continueToSubmit.isDisplayed());
 		continueToSubmit.click();
 		log.info("Submit Agreement.");
 	}
 
 	/**
 	 * This method will click on Pay option.
+	 * @return void
+	 * @param void
 	 */
-	@Step("clicking on pay option...")
+	@Step("click on pay option")
 	public static void pay() {
 		Assert.assertTrue(pay.isDisplayed());
 		pay.click();
@@ -239,11 +272,13 @@ public class ApplicationPageForQuotation {
 	}
 
 	/**
-	 * This method will observ basic fees and Quotation amount
+	 * This method will observe basic fees and Quotation amount
+	 * @return void
+	 * @param void
 	 */
-	@Step("Observing basic fees and Quotation amount...")
-	public static void observFees() {
-		Assert.assertTrue(basicFees.isDisplayed());
+	@Step("Verify observe basic fees and Quotation amount")
+	public static void observeBasicFees() {
+
 		String basicFee = basicFees.getAttribute("Value");
 		basicFee = basicFee.substring(0, basicFee.indexOf("."));
 		String quotAmount = quotationAmount.getAttribute("Value");
@@ -256,16 +291,32 @@ public class ApplicationPageForQuotation {
 	}
 
 	/**
-	 * This method will search for coupon option for application which is
-	 * applied through Quotation
+	 * This method will search for coupon option for application which is applied through Quotation
+	 * @return void
+	 * @param void
 	 */
-	@Step("Searching for coupon option for application which is applied through Quotation...")
+	@Step("Verify Search for coupon option for application which is applied through Quotation")
 	public static void couponOption() {
-		Assert.assertTrue(basicFees.isDisplayed());
 		if (Setup.driver.getPageSource().contains("Do you have a coupon?")) {
 			log.info("Coupon Discount available for Quotation.");
 		} else {
 			log.info("Coupon Discount is not available for Quotation.");
 		}
 	}
+	
+	/**
+	 * 
+	 * This method will Verify customer tries to make payment after the expiration date of the quote unable to make the payment.
+	 * @return void
+	 * @param void
+	 */
+	@Step("Verify customer tries to make payment after the expiration date of the quote unable to make the payment")
+	public static void payForExpiredQuote() {
+		Assert.assertTrue(payNow.isDisplayed());
+		payNow.click();
+		log.info("pay for expired Application.");
+		
+	}
+
 }
+
